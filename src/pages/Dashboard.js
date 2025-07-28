2dc285b7abc8cb060e93ab2d56bf64b5930446b5
sijoow <leshwann@gmail.com>
2025-07-28 17:18:13 +0900

    ds

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -300,10 +300,10 @@ export default function Dashboard() {
                 bodyStyle={{ padding: 16, height: '100%' }} // 본문은 카드 높이 전부 사용
                 loading={loading}
               >
-            <Space size="large" style={{ marginBottom: 16,  }}>
-              <Statistic title="발급 쿠폰" value={couponTotals.issued} suffix="개"style={{fontSize:'14px',textAlign:'center'}}/>
-              <Statistic title="사용 쿠폰" value={couponTotals.used}   suffix="개"  style={{fontSize:'14px',textAlign:'center'}}/>
-              <Statistic title="미사용 쿠폰" value={couponTotals.unused} suffix="개" style={{fontSize:'14px',textAlign:'center'}} />
+            <Space size="large" style={{ marginBottom: 16,justifyContent: 'center'  }}>
+              <Statistic title="발급 쿠폰" value={couponTotals.issued} suffix="개"  valueStyle={{ fontSize: 18 }} />
+              <Statistic title="사용 쿠폰" value={couponTotals.used}   suffix="개"  valueStyle={{ fontSize: 18 }}  />
+              <Statistic title="미사용 쿠폰" value={couponTotals.unused} suffix="개"  valueStyle={{ fontSize: 18 }} />
             </Space>
             <Table
               size="small"
