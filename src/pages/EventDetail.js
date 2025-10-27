9250b4887d18e7496ec153d69dd1b2f6c96485ce
이승환 <leshwann@gmail.com>
2025-10-27 22:20:49 +0900

    main

--- src/pages/EventDetail.js
+++ src/pages/EventDetail.js
@@ -82,7 +82,7 @@ export default function EventDetail() {
       return;
     }
 
-    let html = `<!--@layout(/layout/basic/main.html)-->\n\n`;
+    let html = `<!--@layout(/layout/basic/layout.html)-->\n\n`;
     html += `<div id="evt-root"></div>\n\n`;
 
     const allBlocks = event.content?.blocks || [];
